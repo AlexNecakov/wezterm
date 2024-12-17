cbc1a7a2dc24e81774d9d46f20fb9c5e328e76bf
AlexNecakov <alexnecakov@gmail.com>
2024-12-17 17:19:17 -0500

    set pwsh for windows

--- launchmenu.lua
+++ launchmenu.lua
@@ -1,31 +1,8 @@
 local wezterm = require 'wezterm'
 local launch_menu = {}
 
-if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-    table.insert(launch_menu, {
-        label = 'PowerShell',
-        args = { 'pwsh.exe', '-NoLogo' },
-    })
-
-    -- Find installed visual studio version(s) and add their compilation
-    -- environment command prompts to the menu
-    for _, vsvers in
-    ipairs(
-        wezterm.glob('Microsoft Visual Studio/20*', 'C:/Program Files (x86)')
-    )
-    do
-        local year = vsvers:gsub('Microsoft Visual Studio/', '')
-        table.insert(launch_menu, {
-            label = 'x64 Native Tools VS ' .. year,
-            args = {
-                'cmd.exe',
-                '/k',
-                'C:/Program Files (x86)/'
-                .. vsvers
-                .. '/BuildTools/VC/Auxiliary/Build/vcvars64.bat',
-            },
-        })
-    end
+if wezterm.target_triple:find("windows") then
+    launch_menu.default_prog = { 'pwsh.exe' }
 end
 
 return {
