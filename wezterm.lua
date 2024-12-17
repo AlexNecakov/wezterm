cad25f1a12ac7463e0fec98bd569bddddee96112
AlexNecakov <alexnecakov@gmail.com>
2024-12-17 17:19:09 -0500

    set theme for streaming

--- wezterm.lua
+++ wezterm.lua
@@ -1,11 +1,9 @@
 local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
-config.color_scheme = 'Tokyo Night Storm'
-
-config.font =
-    wezterm.font('JetBrains Mono')
-config.font_size = 17
+config.color_scheme = 'cyberpunk'
+config.window_background_opacity = 0.95
+config.font_size = 34
 
 require 'launchmenu'
 return config
