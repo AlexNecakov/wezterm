003afeb088489cc41fd569d450607d8f78662f5d
Alex Necakov <alexnecakov@gmail.com>
2025-01-02 13:42:48 -0500

    got pwsh working on windows

--- wezterm.lua
+++ wezterm.lua
@@ -5,5 +5,9 @@ config.color_scheme = 'cyberpunk'
 config.window_background_opacity = 0.95
 config.font_size = 34
 
-require 'launchmenu'
+if wezterm.target_triple == ("x86_64-pc-windows-msvc") then
+    config.default_prog = { 'pwsh.exe' }
+    config.font_size = 20
+end
+
 return config
