f77997e35dacc6b74fb57d530e0b21e4184b389c
Alex Necakov <alexnecakov@gmail.com>
2026-02-28 17:44:56 -0500

    linux cond

--- wezterm.lua
+++ wezterm.lua
@@ -3,11 +3,14 @@ local config = wezterm.config_builder()
 
 config.color_scheme = 'cyberpunk'
 config.window_background_opacity = 0.95
-config.font_size = 34
 
 if wezterm.target_triple == ("x86_64-pc-windows-msvc") then
     config.default_prog = { 'pwsh.exe' }
     config.font_size = 20
+elseif wezterm.target_triple:find "linux" then
+    config.font_size = 20
+else
+    config.font_size = 34
 end
 
 return config
