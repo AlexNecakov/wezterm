d524416d85e432c8f70bef27738626720473e1c2
AlexNecakov <alexnecakov@gmail.com>
2026-02-28 18:48:43 -0500

    added auto fullscreen

--- wezterm.lua
+++ wezterm.lua
@@ -4,13 +4,21 @@ local config = wezterm.config_builder()
 config.color_scheme = 'cyberpunk'
 config.window_background_opacity = 0.95
 
-if wezterm.target_triple == ("x86_64-pc-windows-msvc") then
+config.window_decorations = "NONE"
+
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
     config.default_prog = { 'pwsh.exe' }
     config.font_size = 20
 elseif wezterm.target_triple:find "linux" then
-    config.font_size = 20
+    config.font_size = 24
 else
+    -- macOS or others
     config.font_size = 34
 end
+
+wezterm.on('gui-startup', function(cmd)
+    local _, _, window = wezterm.mux.spawn_window(cmd or {})
+    window:gui_window():toggle_fullscreen()
+end)
 
 return config
