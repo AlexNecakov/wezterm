78bd2fe60f99ea46e510a86c1a3556f887afb225
AlexNecakov <alexnecakov@gmail.com>
2026-03-04 10:26:34 -0500

    fixed fullscreen startup

--- wezterm.lua
+++ wezterm.lua
@@ -10,7 +10,7 @@ if wezterm.target_triple == "x86_64-pc-windows-msvc" then
     config.default_prog = { 'pwsh.exe' }
     config.font_size = 20
 elseif wezterm.target_triple:find "linux" then
-    config.font_size = 24
+    config.font_size = 20
 else
     -- macOS or others
     config.font_size = 34
@@ -18,7 +18,7 @@ end
 
 wezterm.on('gui-startup', function(cmd)
     local _, _, window = wezterm.mux.spawn_window(cmd or {})
-    window:gui_window():toggle_fullscreen()
+    window:gui_window():maximize()
 end)
 
 return config
