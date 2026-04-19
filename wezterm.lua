a4f186aca43a7cfa94488a2443c3431c699ecd2f
Alex Necakov <alexnecakov@gmail.com>
2026-04-19 16:20:32 -0400

    trying maximize again

--- wezterm.lua
+++ wezterm.lua
@@ -4,15 +4,12 @@ local config = wezterm.config_builder()
 config.color_scheme = 'cyberpunk'
 config.window_background_opacity = 0.95
 
-config.window_decorations = "NONE"
-
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
     config.default_prog = { 'pwsh.exe' }
     config.font_size = 20
 elseif wezterm.target_triple:find "linux" then
     config.font_size = 20
 else
-    -- macOS or others
     config.font_size = 34
 end
 
